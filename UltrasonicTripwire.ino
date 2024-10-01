f696c1ca50dabd3be19ec4adcc778754ec65d9dc
sando168 <rigo.sand125@gmail.com>
2024-09-30 21:29:49 -0500

    Updated ISR to execute code 32 timesfaster. Added prototype function for reducing ATMega328p power consumption.

--- UltrasonicTripwire.ino
+++ UltrasonicTripwire.ino
@@ -1,6 +1,22 @@
 volatile uint8_t cntT2 = 0;
 
+void power_reduction() {
+
+  // PRR = B10001101;       //TWI:  Off   TIM2:   On   TIM0:  On    TIM1:   Off
+  //                         //SPI:  Off   USART:  On   ADC:   Off
+  // Serial.println(PRR);
+
+  // TWI:   Disabled
+  // TIM2:  Enabled
+  // TIM0:  Enabled
+  // TIM1:  Enabled
+  // SPI:   Disabled
+  // USART: Enabled
+  // ADC:   Enabled
+}
+
 void init_TIMER2() {
+  // check if timer2 works by printing to Serial Monitor
   cli();                 // disable all interrupts
   
   TCCR2A = B00000000;    //COM2A:  N/A    COM2B: N/A    WGM2:  Normal
@@ -8,8 +24,8 @@ void init_TIMER2() {
   TCNT2  = 0;
   OCR2A  = 0;
   OCR2B  = 0;
-  TIMSK2 = B00000001;    //OCIE2B: N/A    OCIE2A: N/A   TOIE2:  Enabled  
-  TIFR2  = B00000000;    //OCF2B:  N/A    OCF2A:  N/A   TOV2:   N/A
+  TIMSK2 = B00000001;    //OCIE2B: N/A   OCIE2A: N/A   TOIE2:  Enabled  
+  TIFR2  = B00000000;    //OCF2B:  N/A   OCF2A:  N/A   TOV2:   N/A
   ASSR   = B00000000;
   GTCCR  = B00000000;
 
@@ -28,7 +44,7 @@ void init_TIMER2() {
 
 ISR(TIMER2_OVF_vect) {
   cntT2++;
-  if (!cntT2) {
+  if (!(cntT2%8)) {
     Serial.println("Tick...");
   }
 }
@@ -43,4 +59,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // TODO: Check for any reset causes
 }

4c2e9c5d963f36225e038b6a8de36c3a776ea782
sando168 <rigo.sand125@gmail.com>
2024-10-05 23:54:24 -0500

    Added prototype Timer 1 Input Capture functionality for Ultrasonic sensor ECHO pin.

--- UltrasonicTripwire.ino
+++ UltrasonicTripwire.ino
@@ -1,8 +1,10 @@
+#define ECHO_PIN 14
+
 volatile uint8_t cntT2 = 0;
 
 void power_reduction() {
 
-  // PRR = B10001101;       //TWI:  Off   TIM2:   On   TIM0:  On    TIM1:   Off
+  // PRR = B10001101;       //TWI:  Off   TIM2:   On   TIM0:  On    TIM1:   On
   //                         //SPI:  Off   USART:  On   ADC:   Off
   // Serial.println(PRR);
 
@@ -15,12 +17,42 @@ void power_reduction() {
   // ADC:   Enabled
 }
 
+void init_GPIO() {
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void init_TIMER1() {
+  cli();
+
+  TCCR1A = B00000000;    //COM1A:  N/A        COM1B: N/A      WGM1:  Normal
+  TCCR1B = B01000111;    //ICNC1:  Enabled    ICES1: +Edge    WGM1:  Normal    CS1: clk/1024
+  TCCR1C = B00000000;    //FOC1A:  N/A        FOC1B: N/A
+  TCNT1  = 0;
+  OCR1A  = 0;
+  OCR1B  = 0;
+  ICR1   = 0;
+  TIMSK1 = B00100001;    //ICIE1:  Enabled  OCIE1B: N/A   OCIE1A: N/A   TOIE1:  Enabled
+  TIFR1  = B00000000;    //OCF1B:  N/A      OCF1A:  N/A   TOV1:   N/A
+
+  Serial.println(TCCR1A);
+  Serial.println(TCCR1B);
+  Serial.println(TCCR1C);
+  Serial.println(TCNT1);
+  Serial.println(OCR1A);
+  Serial.println(OCR1B);
+  Serial.println(ICR1);
+  Serial.println(TIMSK1);
+  Serial.println(TIFR1);
+
+  sei();
+}
+
 void init_TIMER2() {
   // check if timer2 works by printing to Serial Monitor
   cli();                 // disable all interrupts
   
   TCCR2A = B00000000;    //COM2A:  N/A    COM2B: N/A    WGM2:  Normal
-  TCCR2B = B00000111;    //FOC2A:  N/A    FOC2B: N/A    WGM2:  Normal    CS0: clk/1024
+  TCCR2B = B00000111;    //FOC2A:  N/A    FOC2B: N/A    WGM2:  Normal    CS2: clk/1024
   TCNT2  = 0;
   OCR2A  = 0;
   OCR2B  = 0;
@@ -29,32 +61,38 @@ void init_TIMER2() {
   ASSR   = B00000000;
   GTCCR  = B00000000;
 
-  Serial.println(TCCR2A);
-  Serial.println(TCCR2B);
-  Serial.println(TCNT2);
-  Serial.println(OCR2A);
-  Serial.println(OCR2B);
-  Serial.println(TIMSK2);
-  Serial.println(TIFR2);
-  Serial.println(ASSR);
-  Serial.println(GTCCR);
+  // Serial.println(TCCR2A);
+  // Serial.println(TCCR2B);
+  // Serial.println(TCNT2);
+  // Serial.println(OCR2A);
+  // Serial.println(OCR2B);
+  // Serial.println(TIMSK2);
+  // Serial.println(TIFR2);
+  // Serial.println(ASSR);
+  // Serial.println(GTCCR);
 
   sei();                  // enable all interrupts
+}
+
+ISR(TIMER1_CAPT_vect) {
+  Serial.println("Capture!");
 }
 
 ISR(TIMER2_OVF_vect) {
   cntT2++;
   if (!(cntT2%8)) {
-    Serial.println("Tick...");
+    // Serial.println("Tick...");
   }
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  init_GPIO();
   Serial.println("Before init.");
-  init_TIMER2();
+  init_TIMER1();
   Serial.println("Init complete.");
+  init_TIMER2();
 }
 
 void loop() {

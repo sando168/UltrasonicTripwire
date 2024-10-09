18ef0cb116732d4846cf79c3f2f224a5c80521c4
sando168 <rigo.sand125@gmail.com>
2024-10-08 19:54:09 -0500

    Testing prototype Timer 1 Input Capture Edge Select toggling

--- UltrasonicTripwire.ino
+++ UltrasonicTripwire.ino
@@ -1,6 +1,7 @@
 #define ECHO_PIN 14
+#define TEST_PIN 10
 
-volatile uint8_t cntT2 = 0;
+#define IC1_EDGESEL B01000000;
 
 void power_reduction() {
 
@@ -18,6 +19,8 @@ void power_reduction() {
 }
 
 void init_GPIO() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TEST_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
 
@@ -25,24 +28,24 @@ void init_TIMER1() {
   cli();
 
   TCCR1A = B00000000;    //COM1A:  N/A        COM1B: N/A      WGM1:  Normal
-  TCCR1B = B01000111;    //ICNC1:  Enabled    ICES1: +Edge    WGM1:  Normal    CS1: clk/1024
+  TCCR1B = B11000101;    //ICNC1:  Enabled    ICES1: +Edge    WGM1:  Normal    CS1: clk/1024
   TCCR1C = B00000000;    //FOC1A:  N/A        FOC1B: N/A
   TCNT1  = 0;
   OCR1A  = 0;
   OCR1B  = 0;
   ICR1   = 0;
-  TIMSK1 = B00100001;    //ICIE1:  Enabled  OCIE1B: N/A   OCIE1A: N/A   TOIE1:  Enabled
+  TIMSK1 = B00100000;    //ICIE1:  Enabled  OCIE1B: N/A   OCIE1A: N/A   TOIE1:  N/A
   TIFR1  = B00000000;    //OCF1B:  N/A      OCF1A:  N/A   TOV1:   N/A
 
-  Serial.println(TCCR1A);
-  Serial.println(TCCR1B);
-  Serial.println(TCCR1C);
-  Serial.println(TCNT1);
-  Serial.println(OCR1A);
-  Serial.println(OCR1B);
-  Serial.println(ICR1);
-  Serial.println(TIMSK1);
-  Serial.println(TIFR1);
+  // Serial.println(TCCR1A);
+  // Serial.println(TCCR1B);
+  // Serial.println(TCCR1C);
+  // Serial.println(TCNT1);
+  // Serial.println(OCR1A);
+  // Serial.println(OCR1B);
+  // Serial.println(ICR1);
+  // Serial.println(TIMSK1);
+  // Serial.println(TIFR1);
 
   sei();
 }
@@ -75,19 +78,27 @@ void init_TIMER2() {
 }
 
 ISR(TIMER1_CAPT_vect) {
-  Serial.println("Capture!");
+  // Toggle IC Edge
+  TCCR1B ^= IC1_EDGESEL;
+  Serial.println(ICR1);
 }
 
+volatile uint8_t cntT2 = 0;
+volatile bool pinState;
 ISR(TIMER2_OVF_vect) {
   cntT2++;
-  if (!(cntT2%8)) {
-    // Serial.println("Tick...");
+  if (!(cntT2%64)){//!(cntT2%8)) {
+    Serial.println("Tick...");
+    pinState = !pinState;
+    digitalWrite(TEST_PIN, pinState);
   }
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println(MCUSR);
+
   init_GPIO();
   Serial.println("Before init.");
   init_TIMER1();
